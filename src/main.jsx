3aca5c78cfaf95ef40c11b1d90b667ca1cc811b3
aminul574 <a.soton7@gmail.com>
2024-01-12 18:19:54 +0600

    React query init

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,17 @@ import App from "./App.jsx";
 import "./index.css";
 import { AuthProvider } from "./ContextApi/AuthContext";
 import { BlogProvider } from "./ContextApi/BlogContext";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
+  <QueryClientProvider client={queryClient}>
     <BlogProvider>
       <AuthProvider>
         <App />
       </AuthProvider>
     </BlogProvider>
+  </QueryClientProvider>
   // </React.StrictMode>
 );
